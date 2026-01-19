08952d753c1dbfe5f4b89a82e683e247a4a62be5
niirch <yourshouvo1@gmail.com>
2026-01-19 21:31:27 +0600

    home

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -2,7 +2,7 @@
 import { Navigate, useLocation } from "react-router";
 import useAuth from "../hooks/useAuth";
 
-
+//privateRoute.........
 const PrivateRoute = ({ children }) => {
     const { user, loading } = useAuth();
     const location = useLocation();
